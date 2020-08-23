59a18f49827bebfb96f725ea08b27fc0f0f3016e
Nguyễn Hải Nam <59411728+NguyenHaiNam24082000@users.noreply.github.com>
2020-08-23 17:44:54 +0700

    Add files via upload

---
+++ precache-manifest.2212bc4c1177e4e0fbb079035014e049.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11a3e44f0648c8fceb4994c2f8509d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469439580d0bbd09b2a4",
+    "url": "/static/css/main.0f455cb1.chunk.css"
+  },
+  {
+    "revision": "b6c34afd5252380ab72e",
+    "url": "/static/js/2.1acaf822.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1acaf822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469439580d0bbd09b2a4",
+    "url": "/static/js/main.d6eefc88.chunk.js"
+  },
+  {
+    "revision": "f759e13b0900dcbf7e25",
+    "url": "/static/js/runtime-main.0752f680.js"
+  },
+  {
+    "revision": "5db8bbfb6a548babd3c88584bc301dd2",
+    "url": "/static/media/Bemio.5db8bbfb.otf"
+  },
+  {
+    "revision": "d15613cdc186717b8c23f02392ce876b",
+    "url": "/static/media/CloisterBlack-Light.d15613cd.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "a46451a6ffb37ebed2b644ee24c3ffe0",
+    "url": "/static/media/ProximaNova-Regular.a46451a6.ttf"
+  },
+  {
+    "revision": "dbe80701470368594d02cbe657cfcfe5",
+    "url": "/static/media/Sniglet-Regular.dbe80701.otf"
+  },
+  {
+    "revision": "68a226b4ea5031f07f3d3e0be4bb1653",
+    "url": "/static/media/bg-hero.68a226b4.jpg"
+  },
+  {
+    "revision": "f5b1f9a4f9fc6705e942b8763035a63f",
+    "url": "/static/media/photo1.f5b1f9a4.jpg"
+  },
+  {
+    "revision": "8ddf1224de8a587b27abb88333251086",
+    "url": "/static/media/photo2.8ddf1224.jpg"
+  },
+  {
+    "revision": "309de1dcac496c8a8540176ee3996fd7",
+    "url": "/static/media/photo3.309de1dc.jpg"
+  }
+]);
